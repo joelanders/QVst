343f4244370484e01c86a0ed85f56e4ea8f71e81
Joe Landers <joe@joelanders.net>
2021-04-04 17:14:10 +0200

    building a macos bundle

--- cmake/build_vst_plugin.cmake
+++ cmake/build_vst_plugin.cmake
@@ -6,8 +6,17 @@ endif()
 include(parse_src)
 
 # Build static library
-add_library(${PROJECT_NAME} SHARED ${SRC_CPP} ${SRC_C} ${SRC_H} ${SRC_H_PRIVATE} ${SRC_RES} ${QVST_PLUGIN_DLLMAIN_SRC})
-target_link_libraries(${PROJECT_NAME} ${DEPENDS})
+# add_library(${PROJECT_NAME} SHARED ${SRC_CPP} ${SRC_C} ${SRC_H} ${SRC_H_PRIVATE} ${SRC_RES} ${QVST_PLUGIN_DLLMAIN_SRC})
+# target_link_libraries(${PROJECT_NAME} ${DEPENDS})
+
+set(ALL_SOURCES ${SRC_CPP} ${SRC_C} ${SRC_H} ${SRC_H_PRIVATE} ${SRC_RES} ${QVST_PLUGIN_DLLMAIN_SRC})
+smtg_add_vst3plugin(${PROJECT_NAME} ${ALL_SOURCES})
+
+include(SMTG_Bundle)
+smtg_set_bundle(${PROJECT_NAME}
+    BUNDLE_IDENTIFIER "com.steinberg.vst3.${PROJECT_NAME}"
+    INFOPLIST "${CMAKE_CURRENT_LIST_DIR}/resource/Info.plist" PREPROCESS
+)
 
 # Plugin DLL interface definitions
 set(CMAKE_SHARED_LINKER_FLAGS 
